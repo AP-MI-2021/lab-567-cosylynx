3592bda4bbd64babdd0d7a77df91ecc9872834e9
cosylynx <cosmin18cos@gmail.com>
2021-10-27 12:19:45 +0300

    Finished Lab 5.+ lab problem

--- Domain/object.py
+++ Domain/object.py
@@ -8,13 +8,7 @@ def create_object(object_id: int, name, description, purchase_price, location):
     :param location: the purchase place of the object
     :return: an object
     """
-    return {
-        'obj_id': object_id,
-        'name': name,
-        'desc': description,
-        'pur_price': purchase_price,
-        'loc': location
-    }
+    return [object_id, name, description, purchase_price, location]
 
 
 def get_id(object):
@@ -23,7 +17,7 @@ def get_id(object):
     :param object: an object
     :return: the object ID given as a parameter
     """
-    return object['obj_id']
+    return object[0]
 
 
 def get_name(object):
@@ -32,7 +26,7 @@ def get_name(object):
     :param object: object
     :return: the object name given as a parameter
     """
-    return object['name']
+    return object[1]
 
 
 def get_desc(object):
@@ -41,7 +35,7 @@ def get_desc(object):
     :param object: object
     :return: the object description given as a parameter
     """
-    return object['desc']
+    return object[2]
 
 
 def get_pur_price(object):
@@ -50,7 +44,7 @@ def get_pur_price(object):
     :param object: object
     :return: the object purchase price given as a parameter
     """
-    return object['pur_price']
+    return object[3]
 
 
 def get_loc(object):
@@ -59,10 +53,10 @@ def get_loc(object):
     :param object: the object
     :return: the current object location given as a parameter
     """
-    return object['loc']
+    return object[4]
 
 
 def get_str(object):
     return f"The object called {get_name(object)} with id {get_id(object)} located at {get_loc(object)} purchased " \
-           f"at price {get_pur_price(object)} and the following description: {get_desc(object)}"
+           f"at price {get_pur_price(object)} and with the following description: {get_desc(object)}"
 
