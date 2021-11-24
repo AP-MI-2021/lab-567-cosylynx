f866821e8015767c669d4e77feac95b5be7bb916
cosylynx <cosmin18cos@gmail.com>
2021-11-24 11:56:16 +0200

    fixed lab 6 (2 functionalities other than crud)

--- UserInterface/console_ui.py
+++ UserInterface/console_ui.py
@@ -1,5 +1,7 @@
 from Domain.item import get_str
+from Logic.add_string_to_description import append_str_to_price_above
 from Logic.crud import *
+from Logic.move_items import update_items_location
 
 
 def help_dialog():
@@ -7,6 +9,8 @@ def help_dialog():
           "add,<id>,<name>,<description>,<purchase_price>,<location>;\n"
           "remove,<id>;\n"
           "update,<id>,<name>,<description>,<purchase_price>,<location>;\n"
+          "move,<from_location>,<to_location>;\n"
+          "addtodesc,<threshold_price>,<string_to_description>;\n"
           "showall;\n"
           "exit;\n"
           "Use ',' to separate the arguments of a command and ';' to mark the end of the command.\n"
@@ -35,6 +39,10 @@ def run_console_ui():
                     item_list = validate_remove(args, item_list, undo, redo)
                 elif args[0] == "update":
                     item_list = validate_update(args, item_list, undo, redo)
+                elif args[0] == "move":
+                    item_list = validate_move(args, item_list, undo, redo)
+                elif args[0] == "addtodesc":
+                    item_list = validate_addtodesc(args, item_list, undo, redo)
                 elif args[0] == "showall":
                     validate_showall(args, item_list)
                 else:
@@ -75,6 +83,28 @@ def validate_remove(args, item_list, undo, redo):
             print("Argument for item ID should be a valid integer.")
     else:
         print("Command 'remove' expects a single argument.")
+    return item_list
+
+
+def validate_move(args, item_list, undo, redo):
+    if len(args) == 3:
+        try:
+            item_list = update_items_location(item_list, str(args[1]), str(args[2]), undo, redo)
+        except ValueError:
+            print("Argument for item ID should be a valid integer.")
+    else:
+        print("Command 'move' expects 2 arguments.")
+    return item_list
+
+
+def validate_addtodesc(args, item_list, undo, redo):
+    if len(args) == 3:
+        try:
+            item_list = append_str_to_price_above(item_list, int(args[1]), str(args[2]), undo, redo)
+        except ValueError:
+            print("Argument for item ID should be a valid integer.")
+    else:
+        print("Command 'addtodesc' expects 2 arguments.")
     return item_list
 
 
